9d80b2c95939bebe4a44b0e071b83f1f599ca427
nithinDevRaj <nithinraj.dev@gmail.com>
2024-05-12 11:24:31 +0530

    bub bub bub

--- server.js
+++ server.js
@@ -35,15 +35,15 @@ app.listen(port, () => {
   console.log(`sever is running @ port 3000`);
 });
 
-// app.use(express.static(path.join(__dirname, "client/dist")));
+app.use(express.static(path.join(__dirname, "client/dist")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-// });
-
-app.use(express.static(path.join(__dirname, "client", "dist")));
-
-// Route for serving the index.html file
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
+
+// app.use(express.static(path.join(__dirname, "client", "dist")));
+
+// // Route for serving the index.html file
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// });
